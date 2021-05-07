475b18742fd1d81ed9e9e1d934c3d7b77d4271d5
mike <moshea730@gmail.com>
2021-05-07 19:04:48 +0100

    Add my preferred build tools to system

---
+++ system_build/mikes_toolset.sh
@@ -0,0 +1,10 @@
+# mikes_toolset.sh 
+# creator: Mike O'Shea 
+# Updated: 07/05/2021 
+# Carries out the command line install of the tools that I currently
+# find useful when I am working from the command line.
+
+# GNU General Public License, version 3
+
+#!/bin/bash
+sudo apt install rsync tmux tmuxinator build-essential git vim ssh fortune cowsay tig
