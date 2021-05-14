1d142af997f9dd3b1b15d69c78cb5267e7923e3f
mike <moshea730@gmail.com>
2021-05-14 21:15:39 +0100

    minor updates

--- system_build/mikes_toolset.sh
+++ system_build/mikes_toolset.sh
@@ -1,10 +1,12 @@
 # mikes_toolset.sh 
 # creator: Mike O'Shea 
-# Updated: 07/05/2021 
+# Updated: 13/05/2021 
 # Carries out the command line install of the tools that I currently
 # find useful when I am working from the command line.
 
 # GNU General Public License, version 3
 
 #!/bin/bash
-sudo apt install rsync tmux tmuxinator build-essential git vim ssh fortune cowsay tig mc
+sudo apt install -y rsync tmux tmuxinator build-essential git vim ssh fortune cowsay tig mc lsyncd
+
+mkdir ~/.vim_spell
