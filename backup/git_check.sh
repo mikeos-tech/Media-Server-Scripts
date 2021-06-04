20de19e1bb653e7971ab83399171c78ef39ba843
mike <moshea730@gmail.com>
2021-06-04 19:22:42 +0100

    ensures all git repositories are synced with online repository

---
+++ backup/git_check.sh
@@ -0,0 +1,27 @@
+# git_check.sh 
+# creator: Mike O'Shea 
+# Updated: 04/06/2021 
+# Iterates an array of paths pointing to git repositories and ensures
+# everything is included within the repository and that it
+# is updated in GitHub.
+
+# GNU General Public License, version 3
+
+#!/bin/bash
+
+declare -a git_paths=(
+"/documentation/"
+"/scripts/"
+"/var/www/html/"
+"~/.vim_spell"
+"~/projects/backupQ"
+"~/projects/git_sync_project"
+)
+
+# Iterate the string array using for loop
+for val in ${git_paths[@]}; do
+	cd $val
+	git add .
+	git commit -m 'Over Night Update'
+	git push
+   done
